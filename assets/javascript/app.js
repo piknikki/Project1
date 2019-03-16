9ff2d6be543cb1616d7b788cbeb6f4fe0b01455a
kurtisp <kurtis.pace2011@gmail.com>
2019-03-16 12:25:59 -0600

    lil fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,19 @@
 
-// function initMap() {
+
+
+
+$("#search-button").on("click", function () {
+    
+    var searchTitle = "";
+    var city = "";
+    var apiKey = "fN2JT7PZlbQ8jAFoGeun4pAKP8Rg8y5z";
+    var qs = "https://app.ticketmaster.com/discovery/v2/events?" 
+    + "apikey =" + apiKey + "&countryCode=US";
+
+    $.ajax({
+        url: qs,
+        method: "GET"
+    }).then(
         
-//         var location = new google.maps.LatLng(50.0875726, 14.4189987); // declare location in map
-
-//         var mapCanvas = document.getElementById('map'); // where to place the map in the view
-//         var mapOptions = { // basic options for the map
-//             center: location,
-//             zoom: 16,
-//             panControl: false,
-//             mapTypeId: google.maps.MapTypeId.ROADMAP
-//         }
-
-//         // create greyscale styles
-//         var styles = [
-//             {"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]},
-//             {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]},
-//             {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]},
-//             {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]},
-//             {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]},
-//             {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]},
-//             {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]},
-//             {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]},
-//             {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}
-//             ];
-
-//          var map = new google.maps.Map(mapCanvas, mapOptions); // create a new object and pass configurations from above
-//          map.set('styles', styles); // set the greyscale
-      
-//     google.maps.esvent.addDomListener(window, 'load', initMap); // render on page
-
-
+    );
+});
