a3178043d52eb45e5c652c12ec9a9411a8215f90
kurtisp <kurtis.pace2011@gmail.com>
2019-03-16 11:57:59 -0600

    idk

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // function initMap() {
         
@@ -31,35 +30,3 @@
 //     google.maps.esvent.addDomListener(window, 'load', initMap); // render on page
 
 
-=======
-$(function() {
-    function initMap() {
-        var location = new google.maps.LatLng(50.0875726, 14.4189987); // declare location in map
-
-        var mapCanvas = document.getElementById('map'); // where to place the map in the view
-        var mapOptions = { // basic options for the map
-            center: location,
-            zoom: 16,
-            panControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-
-        // create greyscale styles
-        var styles = [
-            {"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]},
-            {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]},
-            {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]},
-            {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]},
-            {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]},
-            {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]},
-            {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]},
-            {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]},
-            {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}
-            ];
-
-        map.set('styles', styles); // set the greyscale
-        var map = new google.maps.Map(mapCanvas, mapOptions); // create a new object and pass configurations from above
-    }
-    google.maps.event.addDomListener(window, 'load', initMap); // render on page
-})
->>>>>>> 854d83ba9a8babbaa62bf567814d6ac4784140f1
