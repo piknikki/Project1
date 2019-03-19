69ff4d5faa35bc080a57371309c76f3ded9835a1
kurtisp <kurtis.pace2011@gmail.com>
2019-03-18 20:21:45 -0600

    fixed a few bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -121,11 +121,7 @@ function clearSearchResults() {
 /**************************************************************************/
 function addEventToBookmarks(_name, _city, _locLat, _locLong, _url, _desc) {
 
-    // increase the number of book marks
-    numBookmarks++;
-
     database.ref().push({
-        numBookmarks: numBookmarks,
         name: _name,
         city: _city,
         locLatitude: _locLat,
@@ -133,134 +129,6 @@ function addEventToBookmarks(_name, _city, _locLat, _locLong, _url, _desc) {
         url: _url,
         desc: _desc
     });
-}
-
-/**************************************************************************/
-/*      event:          firebase.database.on child added                  */
-/*                                                                        */
-/*      purpose:        populate the bookmarks page                       */
-/**************************************************************************/
-firebase.database().ref().on("value", function (_snapshot) {
-
-
-    var i = 0;
-    var rowNum = 0;
-
-    _snapshot.forEach(function (_item) {
-
-        var newCol = $("<div class='pricing-column col-md-4'></div>");
-        var newCard = $("<div class='card'></div>");
-        var newCardHeader = $("<div class='card-header'></div>");
-        var eventTitle = $("<h3>").text(_item.val().name);
-        var eventCity = $("<h2>").text(_item.val().city);
-        var desc = $("<p>").text(_item.val().desc);
-        var linkToTicketsURL = "";
-
-        var newCardBody = $("<div class='card-body'></div>");
-
-        linkToBuyTickets = $("<button id='disp-link-loc' btnid='" + i + "' class= 'btn btn-sm btn-block btn-outline-dark'>Buy</button>");
-       // linkToSavedTickets = $("<button id='disp-save-loc'  btnid='" + i + "' class= 'btn btn-sm btn-block btn-outline-dark'>Save</button>");
-
-        newCardHeader.append(eventTitle);
-        newCard.append(newCardHeader);
-
-        newCardBody.append(eventCity);
-        newCardBody.append(desc);
-      //  newCardBody.append(linkToSavedTickets);
-        newCardBody.append(linkToBuyTickets);
-
-
-        newCard.append(newCardBody);
-
-        newCol.append(newCard);
-
-        // if i % 3 === 0 then there is a multiple of 3 in the current row
-        //  so we need to create a new row
-        if (i === 0) {
-            var newRow = $("<div class='row r" + rowNum + "'></div>");
-            newRow.append(newCol);
-            $("#t").append(newRow);
-
-        }
-        else if (i % 3 === 0) {
-            // create a new row
-            ++rowNum;
-            var newRow = $("<div class='row r" + rowNum + "'></div>");
-            newRow.append(newCol);
-            $("#t").append(newRow);
-
-
-        }
-        else {
-            var temp = ".r" + rowNum;
-            $(temp).append(newCol);
-        }
-        ++i;
-
-    });
-
-});
-
-
-/**************************************************************************/
-/*      function:       loadBookmark                                     */
-/*                                                                        */
-/*      purpose:        load the saved events from the database           */
-/*                                                                        */
-/*      parameters:     none                                              */
-/*                                                                        */
-/*      return:         none                                              */
-/**************************************************************************/
-function loadBookmark(_name, _city, _locLat, _locLong, _url, _desc, _pos, _rowNum) {
-
-    var newCol = $("<div class='pricing-column col-md-4'></div>");
-    var newCard = $("<div class='card'></div>");
-    var newCardHeader = $("<div class='card-header'></div>");
-    var eventTitle =  $("<h3>").text(_name);
-    var eventCity = $("<h2>").text(_city);
-    var desc = $("<p>").text(_desc);
-    var linkToTicketsURL = _url;
-
-    var newCardBody = $("<div class='card-body'></div>");
-
-    linkToBuyTickets = $("<button id='disp-link-loc' btnid='" + _pos + "' class= 'btn btn-sm btn-block btn-outline-dark'>Buy</button>");
-     linkToSavedTickets = $("<button id='disp-save-loc'  btnid='" + _pos + "' class= 'btn btn-sm btn-block btn-outline-dark'>Save</button>");
-
-        newCardHeader.append(eventTitle);
-        newCard.append(newCardHeader);
-
-        newCardBody.append(eventCity);
-        newCardBody.append(desc);
-        newCardBody.append(linkToSavedTickets);
-        newCardBody.append(linkToBuyTickets);
-
-
-        newCard.append(newCardBody);
-
-        newCol.append(newCard);
-
-    // if i % 3 === 0 then there is a multiple of 3 in the current row
-    //  so we need to create a new row
-    if (_pos === 0) {
-        var newRow = $("<div class='row r" + _rowNum + "'></div>");
-        newRow.append(newCol);
-        $("#t").append(newRow);
-
-    }
-    else if (_pos % 3 === 0) {
-        // create a new row
-        ++_rowNum;
-        var newRow = $("<div class='row r" + _rowNum + "'></div>");
-        newRow.append(newCol);
-        $("#t").append(newRow);
-
-
-    }
-    else {
-        var temp = ".r" + _rowNum;
-        $(temp).append(newCol);
-    }
-
 }
 
 /**************************************************************************/
@@ -344,7 +212,7 @@ function createEventLinks() {
         // click event for the event
         linkToSavedTickets.on("click", function () {
 
-            var index = $(this).attr("btnid");
+            var index =  parseInt( $(this).attr("btnid"));
 
             var decription = "";
             var city = "";
